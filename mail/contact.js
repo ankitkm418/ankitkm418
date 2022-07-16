bf073b93029533d173607bab0abcbf290ed02a8b
= <reeta.trickywebsolutions@gmail.com>
2022-07-16 09:31:40 +0530

    Update portfolio

--- mail/contact.js
+++ mail/contact.js
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "contact.php",
+                url: "https://formsubmit.co/ankitkm418@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
